d7f4855e30218fb62c5848f4b4c862a3dd1244c3
Nick Hart <nick.hart.des@gmail.com>
2020-09-30 12:08:11 -0600

    Connect form to store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers';
+import { rootReducer } from './reducers';
 import App from './components/App';
 import './index.css';
 
-const store = createStore(rootReduver, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={ store }>
